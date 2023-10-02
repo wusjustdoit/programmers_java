4d1bddbc8ad635619f82ceeb8b6b3b7479c444a1
wusjustdoit <wusjustdoit@gmail.com>
2023-10-02 20:04:36 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder result = new StringBuilder(my_string);
+        result.replace(s, s + overwrite_string.length(), overwrite_string);
+        return result.toString();
+    }
+}
