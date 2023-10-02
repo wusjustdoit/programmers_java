d4181307e5d47ecdda07ad7cbac074cb18300fd5
wusjustdoit <wusjustdoit@gmail.com>
2023-10-02 20:16:28 +0900

    [unrated] Title: 카운트 업, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List solution(int start_num, int end_num) {
+        List<Integer> answer = new ArrayList<>();
+        for(int i = start_num; i<=end_num;i++){
+            answer.add(i);
+        }
+        return answer;
+    }
+}
