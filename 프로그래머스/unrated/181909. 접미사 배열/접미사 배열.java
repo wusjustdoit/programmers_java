7946c92145d08f246feaead97c588e94724ab6bb
wusjustdoit <wusjustdoit@gmail.com>
2023-10-12 20:30:16 +0900

    [level unrated] Title: 접미사 배열, Time: 0.17 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] suffixes = new String[my_string.length()];
+
+        for (int i=0; i<my_string.length(); i++) {
+            suffixes[i] = my_string.substring(i);
+        }
+        
+        Arrays.sort(suffixes);
+
+        return suffixes;
+    }
+}
