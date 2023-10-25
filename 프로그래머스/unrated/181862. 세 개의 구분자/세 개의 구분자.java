0a0094e38acc85ea44967f1aaccae5d3f87e49c2
wusjustdoit <wusjustdoit@gmail.com>
2023-10-25 21:36:26 +0900

    [level unrated] Title: 세 개의 구분자, Time: 29.09 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String myStr) {
+        List<String> result = new ArrayList<>();
+
+        StringBuilder current = new StringBuilder();
+        for (char c : myStr.toCharArray()) {
+            if (c == 'a' || c == 'b' || c == 'c') {
+                if (current.length() > 0) {
+                    result.add(current.toString());
+                    current = new StringBuilder();
+                }
+            } else {
+                current.append(c);
+            }
+        }
+
+        if (current.length() > 0) {
+            result.add(current.toString());
+        }
+
+        if (result.isEmpty()) {
+            return new String[]{"EMPTY"};
+        } else {
+            return result.toArray(new String[0]);
+        }
+    }
+}
