841dda4695d5427c0eff8bf092ed03d83b46e103
wusjustdoit <wusjustdoit@gmail.com>
2023-11-06 23:19:58 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 2.00 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,6 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.rangeClosed(0,n).filter(value -> value%2 == 1).toArray();
+    }
+}
