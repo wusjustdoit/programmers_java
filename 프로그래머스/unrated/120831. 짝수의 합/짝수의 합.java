04dcc226e6d33ee569922644e871d1d1dc4960fc
wusjustdoit <wusjustdoit@gmail.com>
2023-11-08 22:17:59 +0900

    [level unrated] Title: 짝수의 합, Time: 1.07 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -1,13 +1,8 @@
+import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-
-        for(int i=n; i>0; i--) {
-            if(i%2 == 0) {
-                answer += i;
-            }
-        }
-
-        return answer;
+        return IntStream.rangeClosed(0, n)
+            .filter(e -> e % 2 == 0)
+            .sum();
     }
 }
