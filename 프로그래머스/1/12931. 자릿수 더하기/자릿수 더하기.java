fcb3585fae9cac09a60297efebb92a1d25c85ec1
wusjustdoit <wusjustdoit@gmail.com>
2023-11-11 19:41:11 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String str = String.valueOf(n);
+
+        for(int i=0; i<str.length(); i++) {
+            answer += Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+
+        return answer;
+    }
+}
